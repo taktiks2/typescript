bab6616cf587057c1dd7887e4a8a011bd2f205c5
taktiks2 <deathproof.lee@gmail.com>
2024-02-04 15:55:25 +0900

    add userController

--- express-rest-api/src/app.ts
+++ express-rest-api/src/app.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
+import userController from "./controllers/userController";
 
 const app = express();
+app.use(express.json());
+
+app.use("/users", userController);
 
 app.get("/", (_: Request, res: Response) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
---
+++ express-rest-api/src/controllers/userController.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+import { Router, Request, Response } from "express";
+
+const prisma = new PrismaClient();
+const router = Router();
+
+router.get("/", async (_: Request, res: Response) => {
+  const users = await prisma.users.findMany();
+  res.json({ users });
+});
+
+router.get("/:id", async (req: Request, res: Response) => {
+  const user = await prisma.users.findUnique({
+    where: { id: parseInt(req.params.id) },
+  });
+  res.json({ user });
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  const { name, email, age } = req.body;
+  const user = await prisma.users.create({
+    data: {
+      name,
+      email,
+      age,
+    },
+  });
+  res.json({ user });
+});
+
+router.put("/:id", async (req: Request, res: Response) => {
+  const { name, email, age } = req.body;
+  const user = await prisma.users.update({
+    where: { id: parseInt(req.params.id) },
+    data: { name, email, age },
+  });
+  res.json({ user });
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+  const user = await prisma.users.delete({
+    where: { id: parseInt(req.params.id) },
+  });
+  res.json({ user });
+});
+
+export default router;
