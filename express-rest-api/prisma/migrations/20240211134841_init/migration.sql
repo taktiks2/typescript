51a41f4c26532f2fb1f307d7a56f8d9f76937858
taktiks2 <deathproof.lee@gmail.com>
2024-02-11 23:07:42 +0900

    add posts table

---
+++ express-rest-api/prisma/migrations/20240211134841_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" SERIAL NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
--- express-rest-api/prisma/seed.ts
+++ express-rest-api/prisma/seed.ts
@@ -1,26 +1,46 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
 const prisma = new PrismaClient();
 
 const users = [
-  { name: "Alice", email: "alice@example.com", age: 27 },
-  { name: "Bob", email: "bob@example.com", age: 30 },
-  { name: "Cindy", email: "cindy@example.com", age: 40 },
-  { name: "Takeru", email: "takeru@example.com", age: 29 },
-  { name: "Shiho", email: "shiho@example.com", age: 27 },
+  { username: "Alice", email: "alice@example.com", password: "abcd" },
+  { username: "Bob", email: "bob@example.com", password: "abcd" },
+  { username: "Cindy", email: "cindy@example.com", password: "abcd" },
+];
+
+const posts = [
+  { id: 1, authorId: 1, title: "Hello World", text: "This is my first blog post!", status: "todo" },
+  { id: 2, authorId: 2, title: "Hello World", text: "This is my first blog post!", status: "todo" },
+  { id: 3, authorId: 3, title: "Hello World", text: "This is my first blog post!", status: "todo" },
 ];
 
 async function main() {
   for (const user of users) {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
     const upsertUser = await prisma.users.upsert({
       where: { email: user.email },
       update: {},
       create: {
-        name: user.name,
+        username: user.username,
         email: user.email,
-        age: user.age,
+        password: hashedPassword,
       },
     });
     console.log({ upsertUser });
+  }
+  for (const post of posts) {
+    const upsertPost = await prisma.posts.upsert({
+      where: { id: post.id },
+      update: {},
+      create: {
+        authorId: post.authorId,
+        title: post.title,
+        text: post.text,
+        status: post.status,
+      },
+    });
+    console.log({ upsertPost });
   }
 }
 
