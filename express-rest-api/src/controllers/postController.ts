338bbe94f96c00ce38cc9cd368f3ae0616dab3dc
taktiks2 <deathproof.lee@gmail.com>
2024-02-12 20:36:21 +0900

    add postController

--- express-rest-api/src/controllers/postController.ts
+++ express-rest-api/src/controllers/postController.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { Router, Request, Response } from "express";
 
 const prisma = new PrismaClient();
@@ -16,43 +16,38 @@ router.get("/:id", async (req: Request, res: Response) => {
   return res.json(post);
 });
 
-// router.post("/", async (req: Request, res: Response) => {
-//   const { username, email, password } = req.body;
-//
-//   try {
-//     await prisma.posts.create({
-//       data: {
-//         authorId:
-//         username,
-//         email,
-//         password,
-//       },
-//     });
-//     return res.json({ message: "success" });
-//   } catch (error) {
-//     if (error instanceof Prisma.PrismaClientKnownRequestError) {
-//       if (error.code === "P2002") {
-//         return res.status(400).json({ message: "exists" });
-//       }
-//     }
-//     return res.status(400).json({ message: error });
-//   }
-// });
-//
-// router.put("/:id", async (req: Request, res: Response) => {
-//   const { username, email, password } = req.body;
-//   await prisma.posts.update({
-//     where: { id: parseInt(req.params.id) },
-//     data: { username, email, password },
-//   });
-//   return res.json({ message: "success" });
-// });
-//
-// router.delete("/:id", async (req: Request, res: Response) => {
-//   await prisma.posts.delete({
-//     where: { id: parseInt(req.params.id) },
-//   });
-//   return res.json({ message: "success" });
-// });
-//
+router.post("/", async (req: Request, res: Response) => {
+  const { authorId, title, text, status } = req.body;
+
+  try {
+    await prisma.posts.create({
+      data: {
+        authorId,
+        title,
+        text,
+        status,
+      },
+    });
+    return res.json({ message: "success" });
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+});
+
+router.put("/:id", async (req: Request, res: Response) => {
+  const { authorId, title, text, status } = req.body;
+  await prisma.posts.update({
+    where: { id: parseInt(req.params.id) },
+    data: { authorId, title, text, status },
+  });
+  return res.json({ message: "success" });
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+  await prisma.posts.delete({
+    where: { id: parseInt(req.params.id) },
+  });
+  return res.json({ message: "success" });
+});
+
 export default router;
