da4f69dcca33b1e33d8f74b599e654cb5ac2ce27
taktiks2 <deathproof.lee@gmail.com>
2024-03-03 16:42:29 +0900

    skeltonの導入

--- svelte-todo-app/src/routes/(main)/+layout.svelte
+++ svelte-todo-app/src/routes/(main)/+layout.svelte
@@ -1,6 +1,15 @@
-<nav>
-	<a href="/">Home</a>
-	<a href="/user">User</a>
-</nav>
+<script lang="ts">
+	import { AppShell } from '@skeletonlabs/skeleton';
+</script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">Header</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<nav>
+			<a href="/">Home</a>
+			<a href="/user">User</a>
+		</nav>
+	</svelte:fragment>
+	<slot />
+	<svelte:fragment slot="pageFooter">page footer</svelte:fragment>
+</AppShell>
