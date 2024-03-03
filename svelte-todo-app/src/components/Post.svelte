05eda5248866184fbac64f10df982d78e9ac7334
Takeru Osoegawa <37180466+taktiks2@users.noreply.github.com>
2024-03-03 16:46:04 +0900

    skeltonの導入 (#20)
    * skeltonの導入
    
    * READMEの更新

--- svelte-todo-app/src/components/Post.svelte
+++ svelte-todo-app/src/components/Post.svelte
@@ -12,40 +12,44 @@
 	}
 </script>
 
-<form
-	method="post"
-	use:enhance={() => {
-		return async ({ result }) => {
-			if (result.type === 'success') {
-				await onUpdate();
-				handleModeChange();
-			}
-		};
-	}}
->
-	<input type="hidden" name="id" value={post.id} />
-	<input type="hidden" name="authorId" value={post.authorId} />
-	{#if isEditing}
-		<div>
-			<input type="text" placeholder="Email" name="title" value={post.title} required />
-		</div>
-		<div>
-			<textarea placeholder="Text" name="text" value={post.text} required />
-		</div>
-		<select name="status" value={post.status}>
-			<option value="todo">TODO</option>
-			<option value="progress">PROGRESS</option>
-			<option value="done">DONE</option>
-		</select>
-		<div>
-			<button on:click={handleModeChange}>キャンセル</button>
-			<button formaction="?/edit">更新</button>
-		</div>
-	{:else}
-		<h2>{post.title}</h2>
-		<p>{post.text}</p>
-		<p>{post.status}</p>
-		<button on:click={handleModeChange}>編集</button>
-		<button formaction="?/delete">削除</button>
-	{/if}
-</form>
+<div class="card p-4">
+	<form
+		method="post"
+		use:enhance={() => {
+			return async ({ result }) => {
+				if (result.type === 'success') {
+					await onUpdate();
+					handleModeChange();
+				}
+			};
+		}}
+	>
+		<input type="hidden" name="id" value={post.id} />
+		<input type="hidden" name="authorId" value={post.authorId} />
+		{#if isEditing}
+			<div>
+				<input type="text" placeholder="Email" name="title" value={post.title} required />
+			</div>
+			<div>
+				<textarea placeholder="Text" name="text" value={post.text} required />
+			</div>
+			<select name="status" value={post.status}>
+				<option value="todo">TODO</option>
+				<option value="progress">PROGRESS</option>
+				<option value="done">DONE</option>
+			</select>
+			<div>
+				<button type="button" class="btn variant-filled" on:click={handleModeChange}
+					>キャンセル</button
+				>
+				<button type="button" class="btn variant-filled" formaction="?/edit">更新</button>
+			</div>
+		{:else}
+			<h2>{post.title}</h2>
+			<p>{post.text}</p>
+			<p>{post.status}</p>
+			<button type="button" class="btn variant-filled" on:click={handleModeChange}>編集</button>
+			<button type="button" class="btn variant-filled" formaction="?/delete">削除</button>
+		{/if}
+	</form>
+</div>
