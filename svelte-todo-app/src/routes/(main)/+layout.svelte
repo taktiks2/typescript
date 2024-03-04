54f10a4c9ea2024626e3d1a2ff95a775a438519f
taktiks2 <deathproof.lee@gmail.com>
2024-03-04 23:11:31 +0900

    サイドバーのUI

--- svelte-todo-app/src/routes/(main)/+layout.svelte
+++ svelte-todo-app/src/routes/(main)/+layout.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
-	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppShell, AppRail, AppRailAnchor } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+
+	function handleLogout() {}
 </script>
 
 <AppShell>
 	<svelte:fragment slot="header">Header</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
-		<nav>
-			<a href="/">Home</a>
-			<a href="/user">User</a>
-		</nav>
+		<AppRail>
+			<AppRailAnchor href="/" selected={$page.url.pathname === '/'}>ホーム</AppRailAnchor>
+			<AppRailAnchor href="/user" selected={$page.url.pathname === '/user'}>ユーザー</AppRailAnchor>
+			<AppRailAnchor href="/login" on:click={handleLogout}>ログアウト</AppRailAnchor>
+		</AppRail>
 	</svelte:fragment>
 	<slot />
 	<svelte:fragment slot="pageFooter">page footer</svelte:fragment>
