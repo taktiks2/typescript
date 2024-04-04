2b530d3e56fc3895e49c736eecc0d77115d91181
taktiks2 <deathproof.lee@gmail.com>
2024-04-05 00:11:59 +0900

    hello world

--- tic-tac-toe-phaser/src/main.ts
+++ tic-tac-toe-phaser/src/main.ts
@@ -1,24 +1,47 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import Phaser from "phaser";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+class TestScene extends Phaser.Scene {
+  constructor() {
+    super("testScene");
+  }
+  preload() {
+    this.load.setBaseURL("https://labs.phaser.io");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+    this.load.image("sky", "assets/skies/space3.png");
+    this.load.image("logo", "assets/sprites/phaser3-logo.png");
+    this.load.image("red", "assets/particles/red.png");
+  }
+  create() {
+    this.add.image(400, 300, "sky");
+
+    const particles = this.add.particles(0, 0, "red", {
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: "ADD",
+    });
+
+    const logo = this.physics.add.image(400, 100, "logo");
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    particles.startFollow(logo);
+  }
+}
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: "app",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200, x: 0 },
+    },
+  },
+  scene: TestScene,
+};
+
+const game = new Phaser.Game(config);
