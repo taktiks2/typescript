2b6694074869dd534a5a22bd4470ad166efe3d3e
taktiks2 <deathproof.lee@gmail.com>
2024-06-16 22:01:43 +0900

    コメント追加

--- vue-todo-app/src/App.vue
+++ vue-todo-app/src/App.vue
@@ -3,6 +3,10 @@ import Count from './components/Count.vue'
 import InputText from './components/InputText.vue'
 import IfCondition from './components/IfCondition.vue'
 import TodoList from './components/TodoList.vue'
+
+// NOTE: step-8までやった
+// TODO: step-9からやる
+// https://ja.vuejs.org/tutorial/#step-9
 </script>
 
 <template>
