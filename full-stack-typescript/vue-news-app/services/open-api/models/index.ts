324beb42565e4edeb27d662f5cec7737ad601553
taktiks2 <deathproof.lee@gmail.com>
2024-08-25 14:28:26 +0900

    open-apiの生成

---
+++ full-stack-typescript/vue-news-app/services/open-api/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './V1PostsIdGet200Response';
+export * from './V1PostsIdPutRequest';
+export * from './V1PostsPostRequest';
+export * from './V1UsersIdGet200Response';
+export * from './V1UsersIdPutRequest';
+export * from './V1UsersPostRequest';
